7651b66e05499f08e59dbbed591acb2fb57ae0b6
M Eshwar <meshwar0696@gmail.com>
2026-01-06 22:18:10 +0530

    Update BubbleSort.java

--- 15-11-2025/BubbleSort.java
+++ 15-11-2025/BubbleSort.java
@@ -6,11 +6,11 @@ public class Main
 	    Scanner sc=new Scanner(System.in);
 	    int[] arr={1,3,2,4,-1,56,0};
 	    for(int i=0;i<arr.length;i++){
-	        for(j=i;j<arr.length;j++){
-	            if(arr[i]>arr[j]){
-	                int temp=arr[i];
-	                arr[i]=arr[j];
-	                arr[j]=temp;
+	        for(int j=0;j<arr.length-1;j++){
+	            if(arr[j]>arr[j+1]){
+	                int temp=arr[j];
+	                arr[j]=arr[j+1];
+	                arr[j+1]=temp;
 	            }
 	        }
 	    }
